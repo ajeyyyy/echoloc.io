ad26ad0c775bc4d522ed4c46ba19216e0109e9b4
ajeyyyy <ajaykumar200995@gmail.com>
2021-03-26 03:08:02 -0500

    Prepared for Deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require("cors");
+const path = require("path");
 
 const connectDB = require('./config/db');
 
@@ -18,7 +19,15 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/post', require('./routes/api/post'));
 app.use('/api/auth', require('./routes/api/auth'));
 
-// app.get('/', (req, res) => res.send('API running'));
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
